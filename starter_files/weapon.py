6dabe89333aed5f735d95a78ccebd00c504934d2
Mohammed <74876877+MohSensei@users.noreply.github.com>
2024-05-15 20:26:28 -0400

    Added the direction of the bow to move according to where the mouse is

--- starter_files/weapon.py
+++ starter_files/weapon.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 class Weapon():
     def __init__(self, image):
         self.original_image = image 
@@ -9,5 +10,11 @@ class Weapon():
     def update(self, player):
         self.rect.center = player.rect.center
 
+        pos = pygame.mouse.get_pos()
+        x_dist = pos[0] - self.rect.centerx
+        y_dist = -(pos[1] - self.rect.centery)#-ve because pygame y coordinates increase down the screen
+        self.angle = math.degrees(math.atan2(y_dist, x_dist))
+
     def draw(self, surface):
+        self.image = pygame.transform.rotate(self.original_image, self.angle)
         surface.blit(self.image, self.rect)
