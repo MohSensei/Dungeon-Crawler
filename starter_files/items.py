814831ef8c0a9d696361359e9134d79f16ccafd9
Mohammed <74876877+MohSensei@users.noreply.github.com>
2024-06-04 16:09:18 -0400

    Added animation to the coins

--- starter_files/items.py
+++ starter_files/items.py
@@ -9,3 +9,16 @@ class Item(pygame.sprite.Sprite):
         self.image = self.animation_list[self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+
+    def update(self):
+        #handle animation
+        animation_cooldown = 150
+        #update image
+        self.image = self.animation_list[self.frame_index]
+        #check if enought time has passed since the last update
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+            self.frame_index += 1
+            self.update_time = pygame.time.get_ticks()
+        #check if animation has finished
+        if self.frame_index >= len(self.animation_list):
+            self.frame_index = 0 
