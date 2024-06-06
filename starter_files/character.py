324480b639e5896c7425ddfbb8d1193af9180e8a
Mohammed <74876877+MohSensei@users.noreply.github.com>
2024-06-06 16:55:29 -0400

    Added a fireball attack that the final boss can shoot

--- starter_files/character.py
+++ starter_files/character.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import weapon
 import constants
 class Character():
     def __init__(self, x, y, health, mob_animations, char_type, boss, size):
@@ -16,6 +17,7 @@ class Character():
         self.alive = True
         self.hit = False
         self.last_hit = pygame.time.get_ticks()
+        self.last_attack = pygame.time.get_ticks()
         self.stunned = False
 
         self.image = self.animation_list[self.action][self.frame_index]
@@ -81,11 +83,12 @@ class Character():
         
         return screen_scroll
     
-    def ai(self, player, obstacle_tiles, screen_scroll):
+    def ai(self, player, obstacle_tiles, screen_scroll, fireball_image):
         clipped_line = ()
         stun_cooldown = 100
         ai_dx = 0
         ai_dy = 0
+        fireball = None
 
         #repostion the mobs based on screen scroll
         self.rect.x += screen_scroll[0]
@@ -121,6 +124,14 @@ class Character():
                     player.health -= 10
                     player.hit = True
                     player.last_hit = pygame.time.get_ticks()
+                #boss enemies shoot fireballs
+                fireball_cooldown = 700
+                if self.boss:
+                    if dist < 500:
+                        if pygame.time.get_ticks() - self.last_attack >= fireball_cooldown:
+                            fireball = weapon.Fireball(fireball_image, self.rect.centerx, self.rect.centery, player.rect.centerx, player.rect.centery)
+                            self.last_attack = pygame.time.get_ticks()
+
 
 
             #check if hit
@@ -133,6 +144,8 @@ class Character():
 
             if (pygame.time.get_ticks() - self.last_hit > stun_cooldown):
                 self.stunned = False
+
+        return fireball
 
 
     def update(self):
@@ -179,4 +192,3 @@ class Character():
             surface.blit(flipped_image, (self.rect.x, self.rect.y - constants.SCALE * constants.OFFSET))
         else:
             surface.blit(flipped_image, self.rect)
-        pygame.draw.rect(surface, constants.RED, self.rect, 1)
--- starter_files/main.py
+++ starter_files/main.py
@@ -15,7 +15,7 @@ pygame.display.set_caption("Dungeon Crawler")
 clock = pygame.time.Clock()
 
 #define game variables
-level = 1
+level = 3
 screen_scroll = [0, 0]
 
 #define player movement variables
@@ -54,6 +54,7 @@ item_images.append(red_potion)
 #load weapon images
 bow_image = scale_image(pygame.image.load("starter_files/assets/images/weapons/bow.png").convert_alpha(), constants.WEAPON_SCALE)
 arrow_image = scale_image(pygame.image.load("starter_files/assets/images/weapons/arrow.png").convert_alpha(), constants.WEAPON_SCALE)
+fireball_image = scale_image(pygame.image.load("starter_files/assets/images/weapons/fireball.png").convert_alpha(), constants.FIREBALL_SCALE)
 
 #load tilemap images
 tile_list = []
@@ -162,6 +163,7 @@ enemy_list = world.character_list
 damage_text_group = pygame.sprite.Group()
 arrow_group = pygame.sprite.Group()
 item_group = pygame.sprite.Group() 
+fireball_group = pygame.sprite.Group()
 
 score_coin = Item(constants.SCREEN_WIDTH - 115, 23, 0, coin_images, True)
 item_group.add(score_coin)
@@ -198,7 +200,9 @@ while run:
     #update all objects
     world.update(screen_scroll)
     for enemy in enemy_list:
-        enemy.ai(player, world.obstacle_tiles, screen_scroll)
+        fireball = enemy.ai(player, world.obstacle_tiles, screen_scroll, fireball_image)
+        if fireball:
+            fireball_group.add(fireball)
         if enemy.alive:
             enemy.update()
     player.update()
@@ -211,6 +215,7 @@ while run:
             damage_text = DamageText(damage_pos.centerx, damage_pos.y, str(damage), constants.RED)
             damage_text_group.add(damage_text)
     damage_text_group.update()
+    fireball_group.update(screen_scroll, player)
     item_group.update(screen_scroll, player)
 
 
@@ -222,6 +227,8 @@ while run:
     bow.draw(screen)
     for arrow in arrow_group:
         arrow.draw(screen)
+    for fireball in fireball_group:
+        fireball.draw(screen)
     damage_text_group.draw(screen)
     item_group.draw(screen)
     draw_info()
