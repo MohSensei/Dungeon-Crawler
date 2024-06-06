8e94468d0f398759642e04bff4efa0775aa03a84
Mohammed <74876877+MohSensei@users.noreply.github.com>
2024-06-06 18:57:39 -0400

    Now the intro fade shows from every dirc

--- starter_files/main.py
+++ starter_files/main.py
@@ -159,7 +159,9 @@ class ScreenFade():
         self.fade_counter += self.speed
         if self.direction == 1:
             pygame.draw.rect(screen, self.color, (0 - self.fade_counter,0, constants.SCREEN_WIDTH // 2, constants.SCREEN_HEIGHT))
-        pygame.draw.rect(screen, self.color, (constants.SCREEN_WIDTH // 2 + self.fade_counter, 0, constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+            pygame.draw.rect(screen, self.color, (constants.SCREEN_WIDTH // 2 + self.fade_counter, 0, constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+            pygame.draw.rect(screen, self.color, (0, 0 - self.fade_counter, constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT // 2))
+            pygame.draw.rect(screen, self.color, (0, constants.SCREEN_HEIGHT // 2 + self.fade_counter, constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
         
         
         if self.fade_counter >= constants.SCREEN_WIDTH:
